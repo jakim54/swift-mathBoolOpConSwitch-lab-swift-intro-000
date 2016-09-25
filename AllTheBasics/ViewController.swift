c3d3619d27d7e5689f7fa89dd097252f19c82e58
jajuki13 <khkim1776@gmail.com>
2016-09-25 15:46:10 -0400

    figured out emojis must be within quotes

--- AllTheBasics/ViewController.swift
+++ AllTheBasics/ViewController.swift
@@ -15,12 +15,41 @@ class ViewController: UIViewController {
         
     }
     
+    func averageIsAbove75(_ arg1: Double, _ arg2: Double, _ arg3: Double) -> Bool {
+        let initialSum = (arg1 + arg2 + arg3) / 3.0
+        if initialSum > Double(75) {
+            return true
+        } else {
+            return false
+        }
+    }
     
+    func passwordCombo(username: String, password: Int) -> String {
+        if username == "Jerry" || username == "Elaine" || username == "Michael" {
+            if password % 3 == 0 {
+                return "Welcome!"
+            } else {
+                return "Access Denied"
+            }
+        } else {
+            return "Access Denied"
+        }
+    }
     
-    
-    // Implement your functions here!
-    
-    
-    
-    
+    func describe(emoji: String) -> String {
+        switch emoji{
+        case "💋":
+            return "Kiss"
+        case "🐈":
+            return "Cat"
+        case "🐢":
+            return "Turtle"
+        case "🍕":
+            return "Pizza"
+        case "👻":
+            return "Ghost"
+        default:
+            return "Unknown"
+        }
+    }
 }
